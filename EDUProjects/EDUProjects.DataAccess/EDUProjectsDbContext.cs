45db43f97533d82e0ccce56aa39b77e80496e799
Andra Serban <andraserban64@yahoo.com>
2020-04-07 19:30:43 +0300

    Added project asp.net core 3.1

---
+++ EDUProjects/EDUProjects.DataAccess/EDUProjectsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EDUProjects.ApplicationLogic.Data
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDUProjects.DataAccess
+{
+    public class EDUProjectsDbContext: DbContext
+    {
+        public EDUProjectsDbContext(DbContextOptions<EDUProjectsDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
