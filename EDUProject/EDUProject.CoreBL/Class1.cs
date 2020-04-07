948d580e0f4af29ef3781b1b79652dff7015891e
Andra Serban <andraserban64@yahoo.com>
2020-04-07 19:29:41 +0300

    Deleted project v ASP .Net 2.1

--- EDUProject/EDUProject.CoreBL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EDUProject.CoreBL
-{
-    public class Class1
-    {
-    }
-}
--- EDUProject/EDUProject.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EDUProject.DataAccess
-{
-    public class Class1
-    {
-    }
-}
