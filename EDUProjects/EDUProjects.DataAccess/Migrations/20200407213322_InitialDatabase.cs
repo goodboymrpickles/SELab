3ae319fed630e68bc3511df011f12f197d2ba628
Andra Serban <andraserban64@yahoo.com>
2020-04-08 00:36:26 +0300

    Database structure

---
+++ EDUProjects/EDUProjects.DataAccess/Migrations/20200407213322_InitialDatabase.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EDUProjects.DataAccess.Migrations
+{
+    public partial class InitialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enrollments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gradings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Grade = table.Column<int>(nullable: false),
+                    Feedback = table.Column<string>(nullable: true),
+                    Is_Passed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gradings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    University = table.Column<string>(nullable: true),
+                    Section = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Birth_Date = table.Column<DateTime>(nullable: false),
+                    Phone_Number = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    EnrollmentId = table.Column<Guid>(nullable: true),
+                    GradingId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Enrollments_EnrollmentId",
+                        column: x => x.EnrollmentId,
+                        principalTable: "Enrollments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Students_Gradings_GradingId",
+                        column: x => x.GradingId,
+                        principalTable: "Gradings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Full_Name = table.Column<string>(nullable: true),
+                    Department = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Birth_Date = table.Column<DateTime>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    GradingId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Teachers_Gradings_GradingId",
+                        column: x => x.GradingId,
+                        principalTable: "Gradings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Subject_Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    EnrollmentId = table.Column<Guid>(nullable: true),
+                    TeacherId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Classes_Enrollments_EnrollmentId",
+                        column: x => x.EnrollmentId,
+                        principalTable: "Enrollments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Classes_Teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Deadline = table.Column<DateTime>(nullable: false),
+                    Requirements = table.Column<string>(nullable: true),
+                    Annexes = table.Column<string>(nullable: true),
+                    Project_Title = table.Column<string>(nullable: true),
+                    ClassId = table.Column<Guid>(nullable: true),
+                    GradingId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Projects_Classes_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "Classes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Projects_Gradings_GradingId",
+                        column: x => x.GradingId,
+                        principalTable: "Gradings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_EnrollmentId",
+                table: "Classes",
+                column: "EnrollmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_TeacherId",
+                table: "Classes",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ClassId",
+                table: "Projects",
+                column: "ClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_GradingId",
+                table: "Projects",
+                column: "GradingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_EnrollmentId",
+                table: "Students",
+                column: "EnrollmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GradingId",
+                table: "Students",
+                column: "GradingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_GradingId",
+                table: "Teachers",
+                column: "GradingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "Enrollments");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+
+            migrationBuilder.DropTable(
+                name: "Gradings");
+        }
+    }
+}
