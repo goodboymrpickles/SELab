8b94d3b5a1c5b143ed98181058c56eaf2060dbd5
Andra Serban <andraserban64@yahoo.com>
2020-04-10 07:37:39 +0300

    Corrected mistake in database

--- EDUProjects/EDUProjects.ApplicationLogic/Data/Grading.cs
+++ EDUProjects/EDUProjects.ApplicationLogic/Data/Grading.cs
@@ -7,7 +7,7 @@ namespace EDUProjects.ApplicationLogic.Data
     public class Grading
     {
         public Guid Id { get; set; }
-        public ICollection<Teacher> Teachers { get; set; }
+        public Teacher Teacher { get; set; }
         public ICollection<Student> Students { get; set; }
         public int Grade { get; set; }
         public string Feedback { get; set; }
