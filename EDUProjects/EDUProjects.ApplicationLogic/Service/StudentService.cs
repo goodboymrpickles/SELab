43744953b75a5f74e97b55895ed5531bc45bdeff
stan98bianca <bianca.stan9898@gmail.com>
2020-04-16 23:32:29 +0300

    Add StudentView

---
+++ EDUProjects/EDUProjects.ApplicationLogic/Service/StudentService.cs
@@ -0,0 +1,34 @@
+﻿using EDUProjects.ApplicationLogic.Abstractions;
+using EDUProjects.ApplicationLogic.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDUProjects.ApplicationLogic.Service
+{
+    public class StudentService
+    {
+        IStudentRepository studentRepository;
+
+        public StudentService(IStudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+
+        public Student GetStudentById(Guid Id)
+        {
+            if (Id == null)
+            {
+                throw new Exception("Null  id");
+            }
+
+            return studentRepository.GetStudentById(Id);
+
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+            return studentRepository.GetAll();
+        }
+    }
+}
---
+++ EDUProjects/EDUProjects/Models/StudentModel/GetAllStudentsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EDUProjects.ApplicationLogic.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDUProjects.Models.StudentModel
+{
+   
+   public class GetAllStudentsViewModel
+    {
+        public IEnumerable<Student> Students { get; set; }
+    }
+}
